2e79536d63e969e8bc37d42e736139d8ee1a247a
Allan Da Silva Pessin <allan.pessin@outlook.com>
2022-01-02 12:55:55 -0300

    finish test controller

--- src/controllers/UserController.test.ts
+++ src/controllers/UserController.test.ts
@@ -2,22 +2,60 @@ import createConnection from "../database";
 import { UserController } from "./UsersControler"
 import { Request } from "express";
 import { makeMockResponse } from "../utils/mocks/mockReponse";
+import { getConnection } from "typeorm";
 
 describe("UserController", () => {
-  it('Deve ser retornado o id do usuairo criado', async () => {
+
+  beforeAll(async () => {
     const connection = await createConnection()
     await connection.runMigrations()
+  })
 
-    const userController = new UserController()
+  afterAll(async () => {
+    const connection = getConnection()
+    await connection.query('DELETE from users')
+    await connection.close()
+  })
+
+  const userController = new UserController()
+  const response = makeMockResponse()
+
+  it('Deve ser retornado status 200 quando o usuário for criado', async () => {
     const request = {
       body: {
         name: "Usuario",
         email: "allan@email.com"
       }
     } as Request
-    
-    const response = makeMockResponse()
 
-    const result =  await userController.createUser(request, response)
+    await userController.createUser(request, response)
+
+    expect(response.state.status).toBe(200)
+  })
+
+  it('Deve retornar status 400 quando nome não for informado', async () => {
+    const request = {
+      body: {
+        name: "",
+        email: "allan@email.com"
+      }
+    } as Request
+
+    await userController.createUser(request, response)
+
+    expect(response.state.status).toBe(400)
+  })
+
+  it('Deve retornar status 400 quando email não for informado', async () => {
+    const request = {
+      body: {
+        name: "Allan",
+        email: ""
+      }
+    } as Request
+
+    await userController.createUser(request, response)
+
+    expect(response.state.status).toBe(400)
   })
 })
--- src/database/index.ts
+++ src/database/index.ts
@@ -3,8 +3,8 @@ import { Connection, createConnection, getConnectionOptions } from "typeorm"
 export default async (): Promise<Connection> => {
   const defaultOption = await getConnectionOptions()
   return createConnection(
-    Object.assign(defaultOption, {
-      databese: process.env.NODE_ENV === 'test' 
+    Object.assign(defaultOption, console.log("database connected!"),  {
+      databese: process.env.NODE_ENV === 'test'
         ? "./src/database/database.test.sqlite"
         : defaultOption.database
     })
